433bb078a57d27881a1022822186ebca12d722c7
Neha Gupta <127198995+Nehaa1108@users.noreply.github.com>
2024-09-27 13:54:25 -0400

    Add files via upload

---
+++ string.sql
@@ -0,0 +1,36 @@
+-- concatination--
+select concat("hey"," ","buddy");
+
+select concat("he","y");
+select concat("hey" ," " ,"you") as pue;
+
+select concat("how" ," " ," are","","you") as ue;
+-- contatination by wordspace(_)--
+select concat_ws("-","hey","you") as lu;
+-- substring
+select substring("hey bon tui kamon?",1);
+
+-- replace--
+SELECT REPLACE("hello buddy","hello","hey");
+
+SELECT REPLACE("hey i love you","love","sarange💕😂");\
+
+-- reverse--
+select REVERSE("hello");
+select REVERSE("pro");
+
+-- uppercase--
+select UPPER("abcdefg");
+select UCASE("abcDefg")
+
+-- lower--
+select lower("ADJHHG");
+
+SELECT LCASE("DGDYGDFFG");
+
+
+-- character length--
+select CHAR_LENGTH("hey");
+
+select CHARACTER_LENGTH("brooo");
+
---
+++ string_function.sql
@@ -0,0 +1,12 @@
+-- insert btw and  5 represent index-0 represent word of wassup for sifting
+select insert('hey wassup ' , 5, 0, 'raju ');
+
+
+
+select left('hey buddy raju', 6);
+
+select right('hey buddy raju', 8);
+
+select repeat('haha hhaha ahha ',5);
+
+select trim(" Alright! ");
